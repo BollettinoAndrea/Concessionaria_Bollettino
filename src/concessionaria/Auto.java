ab62c8b28be2a9af8de8232127e602aad18b377c
bollettino.20159 <bollettinoandrea06@gmail.com>
2024-09-30 12:38:28 +0200

    commit1

---
+++ src/concessionaria/Auto.java
@@ -0,0 +1,23 @@
+package concessionaria;
+
+class Auto {
+    private String marca;
+    private String modello;
+    private double prezzo;
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getModello(){
+        return modello;
+    }
+
+    public double getPrezzo(){
+        return prezzo;
+    }
+
+    public Auto(String marca, String modello, double prezzo){
+
+    }
+}
---
+++ src/concessionaria/Concessionaria_bollettino.java
@@ -0,0 +1,14 @@
+package concessionaria;
+
+public class Concessionaria_bollettino {
+    public static void main(String[] args) {
+
+        String marca = "";
+        String modello = "";
+        double prezzo = 0.0;
+
+        Auto b1 = new Auto(marca, modello, prezzo);
+
+
+    }
+}
